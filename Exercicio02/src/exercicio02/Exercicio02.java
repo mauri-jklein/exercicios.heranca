c75f4ab7a173bba35a0aeb5c544fa87b81c35754
mauri-jklein <mauri-jklein@educar.rs.gov.br>
2023-05-11 21:17:44 -0300

    exercicio 02

--- Exercicio02/src/exercicio02/Exercicio02.java
+++ Exercicio02/src/exercicio02/Exercicio02.java
@@ -5,6 +5,9 @@
  */
 package exercicio02;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author Mauri
@@ -14,11 +17,21 @@ public class Exercicio02 {
     public static void main(String[] args) {
         // TODO code application logic here
         Veiculo veiculo1 = new Carro(5, "VW", "Gol", 2022);
-        Veiculo veiculo2 = new Moto(true, "Honda", "CG125", 2022);
+        Veiculo veiculo2 = new Carro(5, "VW", "Gol", 2022);
+        Veiculo veiculo3 = new Carro(5, "VW", "Gol", 2022);
+        Veiculo veiculo4 = new Carro(5, "VW", "Gol", 2022);
+        Veiculo veiculo5 = new Carro(5, "VW", "Gol", 2022);
+        Veiculo veiculo6 = new Moto(true, "Honda", "CG125", 2022);
+        Veiculo veiculo7 = new Moto(true, "Honda", "CG125", 2022);
+        Veiculo veiculo8 = new Moto(true, "Honda", "CG125", 2022);
+        Veiculo veiculo9 = new Moto(true, "Honda", "CG125", 2022);
+        Veiculo veiculo10 = new Moto(true, "Honda", "CG125", 2022);
         
-        System.out.println(veiculo1.exibirDetalhes());
-        System.out.println(veiculo2.exibirDetalhes());
+        List<Veiculo> veiculos = Arrays.asList(veiculo1, veiculo2, veiculo3, veiculo4, veiculo5, veiculo6, veiculo7, veiculo8, veiculo9, veiculo10);
         
+        veiculos.stream().forEach(v ->{
+            System.out.println(v.exibirDetalhes());
+        });
         
     }
     
