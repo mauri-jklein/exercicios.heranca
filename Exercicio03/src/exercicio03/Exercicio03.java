595ed134934727b361f305c85af4cbfcd7ea84ab
mauri-jklein <mauri-jklein@educar.rs.gov.br>
2023-05-11 22:11:38 -0300

    exercicio 03

--- Exercicio03/src/exercicio03/Exercicio03.java
+++ Exercicio03/src/exercicio03/Exercicio03.java
@@ -5,6 +5,12 @@
  */
 package exercicio03;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
+
 /**
  *
  * @author Mauri
@@ -14,13 +20,19 @@ public class Exercicio03 {
     public static void main(String[] args) {
         // TODO code application logic here
         Produto produto1 = new Livro("Mauri", "Use a Cabeça", 250.23);
-        Produto produto2 = new CD("Zeca Pagodinho", "Vida Boa", 15.23);
+        Produto produto2 = new Livro("Mauri", "Banco de Dados", 250.23);
+        Produto produto3 = new Livro("Mauri", "Java e Spring", 250.23);
+        Produto produto4 = new Livro("Mauri", "Mongo DB", 250.23);
+        Produto produto5 = new Livro("Mauri", "Postgres", 250.23);
+        Produto produto6 = new Livro("Mauri", "JPA e Hibernate", 250.23);
+        Produto produto7 = new Livro("Mauri", "Kubernetes", 250.23);
+        Produto produto8 = new Livro("Mauri", "Docker e Docker Compose", 250.23);
+        Produto produto9 = new CD("Zeca Pagodinho", "Vida Boa", 15.23);
+        List<Produto> produtos = Arrays.asList(produto1, produto2, produto3, produto4, produto5, produto6, produto7, produto8, produto9);
         
-        
-        
-        System.out.println(produto1.exibirDetalhes());
-        System.out.println(produto2.exibirDetalhes());
-        
+        produtos.stream().forEach(p -> {
+            System.out.println(p.exibirDetalhes());
+        });
     }
     
 }
